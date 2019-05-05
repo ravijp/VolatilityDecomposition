e6fdd7ca2737502244875c3266d417d26602ba23
Ravi Prakash <ravijpp@gmail.com>
2019-05-05 16:30:00 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -52,7 +52,7 @@ def main():
 
 if __name__ == '__main__':
 
-    log_file = os.getcwd() + 'VolatilityDecomposition/Logs/run.log'
+    log_file = os.getcwd() + '/VolatilityDecomposition/Logs/run.log'
     if os.path.exists(log_file):
         os.remove(log_file)
 
